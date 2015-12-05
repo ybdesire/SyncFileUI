2ee9b43494dc5201b8e3701c8f7698ac752ca47a
ybdesire <ybdesire@gmail.com>
2015-12-05 20:58:24 +0800

    Completed username & password input validation.

--- app/signin/signin.js
+++ app/signin/signin.js
@@ -13,9 +13,48 @@ angular.module('myApp.signin', ['ngRoute'])
 	$scope.sign_in_btn = function(){
 		console.log($scope.username + ', ' + $scope.password);
 		//username cannot contain the following characters: \/:*?"<>
+		var re = RegExp('[\\\\/:*?"<>]');
+		if($scope.username=='')
+		{
+			$scope.error_msg_username = 'username cannot be blank';
+		}
+		else if(typeof($scope.username)=='undefined')
+		{
+			$scope.error_msg_username = 'username cannot be less than 4 characters';
+		}
+		else if($scope.username.length>50)
+		{
+			$scope.error_msg_username = 'username cannot be more than 50 characters';
+		}
+		else if(re.test($scope.username))
+		{
+			$scope.error_msg_username = 'username cannot contain the following characters: \/:*?"<>';
+		}
+		else
+		{
+			$scope.error_msg_username = '';
+		}
 		
+		if($scope.password=='')
+		{
+			$scope.error_msg_password = 'password cannot be blank';
+		}
+		else if(typeof($scope.password)=='undefined')
+		{
+			$scope.error_msg_password = 'password cannot be less than 4 characters';
+		}
+		else if($scope.password.length>50)
+		{
+			$scope.error_msg_password = 'password cannot be more than 50 characters';
+		}
+		else
+		{
+			$scope.error_msg_password = '';
+		}
+
     };
 	$scope.username = '';
 	$scope.password = '';
-	$scope.error_msg = '';
+	$scope.error_msg_username = '';
+	$scope.error_msg_password = '';
 });
